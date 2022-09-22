b45988f7e53414694cb30778a10ff10e79b8dbbf
Nguyen Tien Dat <dat.nguyendatjet7410@hcmut.edu.vn>
2022-09-22 10:15:49 +0700

    exercise-6-update-error

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -41,7 +41,7 @@ extern "C" {
 
 /* Exported constants --------------------------------------------------------*/
 /* USER CODE BEGIN EC */
-
+#define LED_ALL (GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15);
 /* USER CODE END EC */
 
 /* Exported macro ------------------------------------------------------------*/
